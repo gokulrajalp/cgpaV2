5e38684c13d00662c5a538e35d85d77a145ebf9c
gokulrajalp <gokulrajalp@gmail.com>
2022-07-17 15:52:50 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
+import Lock from './components/Lock';
+import Signin from './components/Signin';
+import Password from './components/Password';
+import Cgpa from './components/cgpa';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Lock/>}/>
+        <Route path='/signin' element={<Signin/>}/>
+        <Route path='/password' element={<Password/>}/>
+        <Route path='/cgpa' element={<Cgpa/>}/>
+
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/Signin.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { useState} from 'react';
+import { useNavigate} from 'react-router-dom';
+import {db} from '../firebase-config'
+import { collection, addDoc } from "firebase/firestore";
+import './signin.css'
+
+
+
+export default function Sign(){
+
+   
+    const [name, setName] = useState();
+   
+  
+    const [mail, setmail] = useState();
+   
+    const [password, setPassword] = useState();
+    const [confirmPassword, setconfirmPassword] = useState();
+    const [regNo] = useState(localStorage.getItem(`regNo`));
+
+
+
+
+
+
+
+    let navigate = useNavigate();
+    function remove(){
+        localStorage.removeItem(`${localStorage.getItem("current")}`);
+        navigate('/');
+    }
+
+    function store(){
+
+        if(!name){
+        alert("enter the name");
+            return;
+        }
+
+    
+         
+        
+                
+             
+                    if(!mail){
+                    alert("enter the mail");
+                            return;
+                    }
+                
+
+
+                
+
+                        if(password!==confirmPassword){
+                             alert("enter the password and confirm password");
+                                return;
+                            }
+
+           
+          
+            
+
+
+            const usersCollectionRef = collection(db, "cgpa");
+const createusers = async () => {
+    await addDoc(usersCollectionRef, {name : name, Password:password, mail : mail, regNo: regNo});
+};
+
+createusers();
+
+
+                            navigate('/password');
+                
+                                                
+    }
+
+
+
+
+
+    return(<div>
+    <div className='container'>
+        <h1>Welcome to the CGPA Calculater</h1>
+        <>{regNo}</><button onClick={remove}>change and delete this Register Number</button>
+<br/><br/>
+
+
+        
+        <h3>Name *</h3><input className='name'  onChange={(e)=>{setName(e.target.value)}}/>
+       
+        
+        <div className='mail'><h3>Mail *</h3><input className='mail_value' onChange={(e)=>{setmail(e.target.value)}}/></div>
+        
+        <h3>New Password *</h3><input className='password' type={"password"} onChange={(e)=>{setPassword(e.target.value)}}/>
+        <h3>Confirm Password *</h3><input className='confirm' type={"password"} onChange={(e)=>{setconfirmPassword(e.target.value)}}/>
+
+        <button onClick={store}>Submit</button>
+
+        </div>
+    </div>
+    );
+}
